4de7e7017448695eb2ff0aaaef536e73fd712237
Programador Novato <info@programadornovato.com>
2019-06-01 11:02:37 -0500

    22.-Vue js 2 tutorial español 👀 [Rutas (router) dinamicas]👀👀

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
-      <router-link to="/caracteristicas">Caracteristicas</router-link>
+      <router-link to="/caracteristicas/1">Caracteristicas</router-link>
     </div>
     <router-view/>
   </div>
--- src/views/Caracteristicas.vue
+++ src/views/Caracteristicas.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="caracteristicas">
     <h1>caracteristicas desde vista</h1>
+    <router-link 
+    v-for="(car,index) of listaCar"
+    :to="{name: 'caracteristicas',params:{id:car}}"
+    :key="index"
+    > {{car}} </router-link>
     <ComCaracteristicas/>
+    <button @click="home" >Home</button>
+    <button @click="atras" >Atras</button>
+    <button @click="adelante" >Adelante</button>
   </div>
 </template>
 
@@ -10,6 +18,22 @@ import ComCaracteristicas from "../components/ComCaracteristicas.vue"
 export default {
   components:{
     ComCaracteristicas
+  },
+  data(){
+    return{
+      listaCar:[1,2,3]
+    }
+  },
+  methods:{
+    home(){
+      this.$router.push("/")
+    },
+    atras(){
+      this.$router.go(-1)
+    },
+    adelante(){
+      this.$router.go(1)
+    },
   }
 }
 </script>
